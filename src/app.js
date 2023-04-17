ab06f898fa8d2dcea64b858572186ee0e30e75e3
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-04-17 02:44:28 -0300

    database setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import { MongoClient } from "mongodb"
 
 // Server 
 const app = express()
@@ -7,6 +8,14 @@ const app = express()
 // Setup
 app.use(express.json())
 app.use(cors())
+
+// Database Setup
+let db;
+const mongoClient = new MongoClient("mongodb://localhost:27017/BatePapoUol")
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
+
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
